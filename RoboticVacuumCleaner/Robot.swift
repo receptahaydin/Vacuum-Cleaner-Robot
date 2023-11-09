91930442355a57ccce5542f069264b6491669719
recepaydin <taha@carex.ai>
2023-11-09 11:20:37 +0300

    updated

--- RoboticVacuumCleaner/Robot.swift
+++ RoboticVacuumCleaner/Robot.swift
@@ -8,6 +8,7 @@
 import Foundation
 
 class Robot {
+    
     var isActive: Bool
     var roomList: [Room]
     
--- RoboticVacuumCleaner/Room.swift
+++ RoboticVacuumCleaner/Room.swift
@@ -8,6 +8,7 @@
 import Foundation
 
 class Room {
+    
     var id: Int
     var isClean: Bool
     
